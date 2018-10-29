d2a7abc342456080610cac3d76bacb1250f4a5cc
Singh-Sid930 <singh.sid930@gmail.com>
2018-10-29 19:56:11 -0400

    added quat-eul publisher

--- scripts/quat-euler.py
+++ scripts/quat-euler.py
@@ -8,7 +8,7 @@ import yaml
 import sys
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import *
+from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
 import pdb
 import csv
@@ -19,42 +19,33 @@ import tf
 # You can define constants in Python as uppercase global names like these.
 x=0
 y=0
-x_old=0
-y_old=0
 quaternion=[]
+pub = rospy.Publisher('odom_array', Vector3, queue_size=10)
+msg=Vector3()
+
 def Position_change(data):
-    qx=data.pose.pose.orientation.x
-    qy=data.pose.pose.orientation.y
-    qz=data.pose.pose.orientation.z
-    qw=data.pose.pose.orientation.w
-    quaternion = (qx,qy,qw,qz)
-    euler = tf.transformations.euler_from_quaternion(quaternion)
-    roll = euler[0]
-    pitch = euler[1]
-    yaw = 180*euler[2]/math.pi
-    #head=180*head/math.pi
-    print(yaw)
-    #euler = data.tf.transformations.euler_from_quaternion(x_q,y_q,z_q,w_q)
-    # roll = euler[0]
-    # pitch = euler[1]
-    # yaw = euler[2]
-    # # global x
-	# global y
-	# global i
-	# global x_old
-	# global y_oldn
-	# x=data.pose.pose.position.x 
-	# y=data.pose.pose.position.y
-	# print(y)
+	global x
+	global y
+	qx=data.pose.pose.orientation.x
+	qy=data.pose.pose.orientation.y
+	qz=data.pose.pose.orientation.z
+	qw=data.pose.pose.orientation.w
+	x=data.pose.pose.position.x
+	y=data.pose.pose.position.y
+	quaternion = (qx,qy,qw,qz)
+	euler = tf.transformations.euler_from_quaternion(quaternion)
+	roll = euler[0]
+	pitch = euler[1]
+	yaw = 180*euler[2]/math.pi
+	msg.x=x
+	msg.y=y
+	msg.z=yaw
+	br = tf.TransformBroadcaster()
+	br.sendTransform((x, y, 0),tf.transformations.quaternion_from_euler(0, 0, yaw),rospy.Time.now(),"car","world")
+	pub.publish(msg)
 
-	# if(i==10):
-	# 	i=0
-	# 	x_old=x
-	# 	y_old=y
-
-	# i=i+1
 
 if __name__ == '__main__':
-  rospy.init_node('pid_error_node', anonymous = True)
+  rospy.init_node('odom_orientation_node', anonymous = True)
   rospy.Subscriber('/vesc/odom',Odometry,Position_change)
   rospy.spin()
